852fdd87f442218ec5d059373bee7c2c727a0965
Jason Tu <title.kubo@gmail.com>
2019-08-22 15:38:01 -0400

    Finished layout + added TODO

---
+++ app/src/main/java/club/gitmad/helloinput/MainActivity.java
@@ -0,0 +1,20 @@
+package club.gitmad.helloinput;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // TODO: Set a listener for btnSubmit. Inside the listener, get the value in etName and set it to tvHello
+    }
+}
