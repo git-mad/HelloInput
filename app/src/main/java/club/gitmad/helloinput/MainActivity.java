41ca863eab16a3d32936041f744a740f2bed230d
Jason Tu <title.kubo@gmail.com>
2020-01-06 11:43:21 -0500

    Added java solution

--- app/src/main/java/club/gitmad/helloinput/MainActivity.java
+++ app/src/main/java/club/gitmad/helloinput/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Set a listener for btnSubmit. Inside the listener, get the value in etName and set it to tvHello
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText etName = findViewById(R.id.etName);
+                String name = etName.getText().toString();
+
+                TextView tvHello = findViewById(R.id.tvHello);
+                tvHello.setText("Hello, " + name + "!");
+            }
+        });
     }
 }
