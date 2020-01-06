de8947257ca380a0f90f057020388564a167e0c7
Jason Tu <title.kubo@gmail.com>
2020-01-06 11:47:26 -0500

    Converted to Kotlin

---
+++ app/src/main/java/club/gitmad/helloinput/MainActivity.kt
@@ -0,0 +1,13 @@
+package club.gitmad.helloinput
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO: Set a listener for btnSubmit. Inside the listener, get the value in etName and set it to tvHello
+    }
+}
